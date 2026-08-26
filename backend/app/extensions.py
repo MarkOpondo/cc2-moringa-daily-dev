0fbb4e862e13d63c36e8c0c28bbacc8e432b6508
Ann-Annita23 <mainaann43241@gmail.com>
2026-08-26 07:55:38 +0300

    Corrected SQLAL to SQLAl

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -10,7 +10,7 @@ metadata = MetaData(naming_convention={
     "fk" : "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
-db=SQLALchemy(metadata=metadata)
+db=SQLAlchemy(metadata=metadata)
 jwt=JWTManager()
 migrate=Migrate()
 cors=CORS()
