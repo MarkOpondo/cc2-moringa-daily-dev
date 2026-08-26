421dfe16e75b7ebb312976cc90c416175f04e16b
Ann-Annita23 <mainaann43241@gmail.com>
2026-08-26 06:47:17 +0300

    Update backend app configuration and extensions

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,11 +1,18 @@
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
+from sqlalchemy import MetaData
 
-db=SQLALchemy()
+metadata = MetaData(naming_convention={
+    "fk" : "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db=SQLALchemy(metadata=metadata)
 jwt=JWTManager()
 migrate=Migrate()
 cors=CORS()
 bcrypt=Bcrypt()
+ma=Marshmallow()
