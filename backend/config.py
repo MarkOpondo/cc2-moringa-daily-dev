3acf151d3fffd86ddb8197a90383b31d0d8ca8b6
Mark Opondo <markopondoi.t@gmail.com>
2026-08-30 17:37:39 +0300

    seed data added

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
-
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASE_DIR, '.env'), override=True)
 
 class Config:
     SECRET_KEY = os.environ.get(
